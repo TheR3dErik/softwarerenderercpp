81d85338907114a904b1484e7eef53479a4fae66
Erik <erik@localhost.localdomain>
2018-09-16 17:40:01 -0400

    Initial commit with some basic code to get me started that I got from stackoverflow

---
+++ cmake-build-debug/CMakeFiles/SDLsoftwarerenderer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SDLsoftwarerenderer.dir/main.cpp.o"
+  "SDLsoftwarerenderer.pdb"
+  "SDLsoftwarerenderer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SDLsoftwarerenderer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
